bbe8af12afea6d754ba45f11ba3dc1f7f37a53f4
alpal920 <147457604+alpal920@users.noreply.github.com>
2024-03-09 15:28:43 -0800

    leetcode mar9

--- leetcodes.js
+++ leetcodes.js
@@ -68,3 +68,52 @@ function objOfIndexes(matrix) {
   }
   return matrixObj;
 }
+
+/*
+https://leetcode.com/problems/minimum-common-value/submissions/1198965520/?envType=daily-question&envId=2024-03-09
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. 
+If there is no common integer amongst nums1 and nums2, return -1.
+
+Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer. 
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return 2.
+Example 2:
+
+Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+Output: 2
+Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so 2 is returned.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 105
+1 <= nums1[i], nums2[j] <= 109
+Both nums1 and nums2 are sorted in non-decreasing order.
+
+loop through array 1 
+if the value is included in array 2, return it 
+both arrays are sorted in ascending order  
+
+loop through nums1 and collect the values 
+loop through nums2, if the value already exists in the object, return it 
+else -1 
+*/
+
+var getCommon = function (nums1, nums2) {
+  let counterObj = {};
+  for (let num of nums1) {
+    if (!(num in counterObj)) {
+      counterObj[num] = true;
+    }
+  }
+  for (let num of nums2) {
+    if (counterObj[num]) {
+      return num;
+    }
+  }
+  return -1;
+};
